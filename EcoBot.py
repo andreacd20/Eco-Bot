b6b8deb5f1ff9df7ced821c5b9fe11f7ed57b5ca
andreacd20 <136090147+andreacd20@users.noreply.github.com>
2023-07-23 19:58:22 +0700

    Update EcoBot.py
    Added bye command, made the lists into a one multi-line message

--- EcoBot.py
+++ EcoBot.py
@@ -17,8 +17,12 @@ async def hello(ctx):
     await ctx.send(f'Hi! I am a bot {bot.user}!')
 
 @bot.command()
+async def bye(ctx):
+    await ctx.send(f'\U0001f44b')
+    
+@bot.command()
 async def info(ctx):
-    await ctx.send(f'Hi! Im Happy Hazel, your friendly eco bot! Im here to teach you ways to live a more eco-friendly lifestyle! Try typing !lifestyle for a start! Remember the Earth is our only home. Lets take care of it properly!')
+    await ctx.send(f'Hi! Im Happy Hazel, your friendly eco bot :)\n \n Im here to teach you ways to live a more eco-friendly lifestyle!\n Remember! Earth is our only home. Lets take care of it properly!\n \n Try typing !lifestyle for a start!')
     img_name = random.choice(os.listdir('5. earthmeme'))
     with open(f'5. earthmeme/{img_name}', 'rb') as f:
         picture = discord.File(f)
@@ -26,15 +30,7 @@ async def info(ctx):
 
 @bot.command()
 async def lifestyle(ctx):
-    await ctx.send(f'Here are a few things you can implement in your lifestyle to help the enviroment!')
-    await ctx.send(f'1. Bring your very own water bottle!')
-    await ctx.send(f'2. Carry a shopping bag around, just in case!')
-    await ctx.send(f'3. Think twice before you buy something!')
-    await ctx.send(f'4. Ditch one time use items from now on!')
-    await ctx.send(f'5. Eat local and organic food!')
-    await ctx.send(f'6. Eat your foooood! Reduce food waste.')
-    await ctx.send(f'7. Ride a bike or go on a walk if its close by! This is also healthier for you :)')
-    await ctx.send(f'8. Type !recycle to see some fun ideas you can transform your waste products into!')
+    await ctx.send(f'Here are a few actions you can implement in your daily life to help the enviroment!\n 1. Bring your very own water bottle!\n 2. Carry a shopping bag around, just in case!\n 3. Think twice before you buy something!\n 4. Ditch one time use items from now on!\n 5. Eat local and organic food!\n 6. Eat your foooood! Reduce food waste.\n 7. Ride a bike or go on a walk if its close by! Its also healthier for you :)\n 8. Type !recycle to see some fun ideas you can transform your waste products into!\n \n \U0001f642 \U0001f642')
 
 @bot.command()
 async def recycle(ctx):
